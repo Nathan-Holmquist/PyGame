1581e00efda930170c13ceb2d0f0894f38a55c63
Nathan-Holmquist <nholmquist50@gmail.com>
2025-01-26 02:13:44 -0500

    today's changes

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
 from sys import exit
 
+score = 0
+deaths = 0
 
 def displayScore():
     timeMili = pygame.time.get_ticks() - start_time
@@ -11,12 +13,14 @@ def displayScore():
 
 
 
+
+score_total = 0
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 screen.fill("blue")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font\\Pixeltype.ttf',50)
-gameOver = True
+gameOver = False
 start_time = 0
 ground = pygame.image.load("graphics\\ground.png").convert()
 backGround = pygame.image.load("graphics\\sky.png").convert()
@@ -41,8 +45,8 @@ player_hitbox = player_surface.get_rect(midbottom = (80,300 + player_gravity))
 
 # Intro Screen
 player_stand = pygame.image.load('graphics\Player\player_stand.png').convert_alpha()
+player_stand = pygame.transform.scale2x(player_stand)
 player_stand_hitbox = player_stand.get_rect(center = (400,200))
-
 
 
 
@@ -63,6 +67,8 @@ while True:
                 start_time = pygame.time.get_ticks()
 
     if not gameOver:
+        
+        print(score)
         screen.blit(backGround, (0,0))
         screen.blit(ground, (0,300))
         # text_surface = test_font.render('Score: '+ str(displayScore()), False, 'Green')
@@ -70,6 +76,7 @@ while True:
         # screen.blit(text_surface, score_hitbox)  
 
         displayScore()
+
 
         snail_hitbox.left -= 6
         if snail_hitbox.right < 0 : snail_hitbox.left = 800
@@ -85,7 +92,30 @@ while True:
             gameOver = True
 
     else: 
+        start_time = pygame.time.get_ticks()
         screen.fill(('red'))
+        screen.blit(player_stand,player_stand_hitbox)
+        deaths += 1
+        game_over_text = test_font.render("Game    Over", False, (64,64,64))
+        game_over_text = pygame.transform.scale2x(game_over_text)
+        game_over_rect = game_over_text.get_rect(center = (400,50))
+
+        play_again_text = test_font.render("Press space bar to play again!", False, (64,64,64))
+        play_again_rect = play_again_text.get_rect(center = (400,350))
+
+        score_text = test_font.render("Score", False, (64,64,64))
+        score_text = pygame.transform.scale2x(score_text)
+        score_rect = score_text.get_rect(center = (150,150))
+
+        numScore_text = test_font.render(str(score_total),False, (64,64,64))
+        numScore_text = pygame.transform.scale2x(numScore_text)
+        numScore_rect = numScore_text.get_rect(center = (150,225))
+
+        screen.blit(play_again_text, play_again_rect)
+        screen.blit(game_over_text,game_over_rect)
+        screen.blit(score_text, score_rect)
+        screen.blit(numScore_text, numScore_rect)
+
 
             
 
